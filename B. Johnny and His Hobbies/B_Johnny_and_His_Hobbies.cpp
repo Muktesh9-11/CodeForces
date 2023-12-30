78e2154bc2700836497dcc4d620524c0316727fc
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2023-12-31 04:28:04 +0530

    Add files via upload

--- B. Johnny and His Hobbies/B_Johnny_and_His_Hobbies.cpp
+++ B. Johnny and His Hobbies/B_Johnny_and_His_Hobbies.cpp
@@ -16,35 +16,31 @@ int main(){
     cin>>n;
     int arr[n];
     vector<int> temp(1024,0);
+
     for(int i=0;i<n;i++){
         cin>>arr[i];
-        temp[arr[i]]++;
+    }
+
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            temp[arr[i]^arr[j]]++;
+        }
     }
 
     int ans=-1;
-    for(int k=1;k<=1023;k++){
-        vector<int> freq(1024,0);
-        for(int i=0;i<n;i++){
-            freq[arr[i]^k]++;
-        }
 
-        bool flag=true;
-        for(int i=0;i<1024;i++){
-            if(temp[i]!=freq[i]){
-                flag=false;
-                break;
-            }
-        }
-
-        if(flag==true){
-            ans=k;
+    for(int i=0;i<1024;i++){
+         if(n%2!=0) {
+        ans=-1;
+        break;
+        
+    }
+        else if(temp[i]==n/2){
+            ans=i;
             break;
         }
     }
-
     cout<<ans<<endl;
-
-   
         
 
 
