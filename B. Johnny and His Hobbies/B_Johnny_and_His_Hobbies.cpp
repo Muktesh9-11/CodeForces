72de71cca36baaaf407d6041aab47163c16c8123
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2023-12-31 04:30:37 +0530

    Add files via upload

--- B. Johnny and His Hobbies/B_Johnny_and_His_Hobbies.cpp
+++ B. Johnny and His Hobbies/B_Johnny_and_His_Hobbies.cpp
@@ -46,3 +46,58 @@ int main(){
 
     }
 }
+
+//Using Brute Force
+/*
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+ 
+//vector<int> answer;
+//string current;
+ 
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t-->0){
+        //code here
+ 
+    int n;
+    cin>>n;
+    int arr[n];
+    vector<int> temp(1024,0);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        temp[arr[i]]++;
+    }
+ 
+    int ans=-1;
+    for(int k=1;k<=1023;k++){
+        vector<int> freq(1024,0);
+        for(int i=0;i<n;i++){
+            freq[arr[i]^k]++;
+        }
+ 
+        bool flag=true;
+        for(int i=0;i<1024;i++){
+            if(temp[i]!=freq[i]){
+                flag=false;
+                break;
+            }
+        }
+ 
+        if(flag==true){
+            ans=k;
+            break;
+        }
+    }
+ 
+    cout<<ans<<endl;
+ 
+   
+        
+ 
+ 
+    }
+}*/
