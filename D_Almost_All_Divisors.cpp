610cd4a4cc31dae27730b4e10e2518fbd8f32443
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2024-05-13 12:41:02 +0530

    starting git

--- D_Almost_All_Divisors.cpp
+++ D_Almost_All_Divisors.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 #define ll long long
 
+void solve(){
+    //code here
+
+
+
+}
+
 int main(){
     int t;
     cin>>t;
     while(t-->0){
-        //code here
-        
-        
-
+        solve();
     }
 }
