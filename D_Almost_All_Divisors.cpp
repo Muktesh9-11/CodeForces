add2541f3dd895ebbaf7c1e9ba90ea63768ada00
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2024-05-14 23:22:20 +0530

    Almost All Divisors CF

--- D_Almost_All_Divisors.cpp
+++ D_Almost_All_Divisors.cpp
@@ -3,17 +3,40 @@
 using namespace std;
 #define ll long long
 
-void solve(){
-    //code here
 
-
-
-}
 
 int main(){
-    int t;
+    ll t;
     cin>>t;
     while(t-->0){
-        solve();
+       // solve();
+    ll n;
+    cin>>n;
+    vector<ll> arr(n);
+    for(ll i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    ll x=arr[0]*arr[n-1];
+    vector<ll> divisors;
+    for (ll i = 2; i*i <= x; i++) {
+        if (x % i == 0) {
+            if(x/i==i){
+            divisors.push_back(i);
+        }
+        else{
+            divisors.push_back(i);
+            divisors.push_back(x/i);
+        } 
+        }
+    }
+    sort(divisors.begin(),divisors.end());
+    
+    if(divisors==arr) cout<<x<<endl;
+        //return;
+    
+    else cout<<-1<<endl;
+        //return;
+    
     }
 }
